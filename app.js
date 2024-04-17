278aacba8be12c6070ec5ce1ede1c97e4667953d
Divyasree Kolanu <divyasreekolanu@Divyasrees-MacBook-Air.local>
2024-04-17 12:33:35 -0400

    updated add.js

---
+++ app.js
@@ -0,0 +1,7 @@
+public class SimpleJavaCode {
+    public static void main(String[] args) {
+        // This program prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
+
